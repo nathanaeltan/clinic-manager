0a53c8bf20e839e4e3c47fd2c27f1011bb1f45f3
Nathanael Tan <nathanael.tan92@gmail.com>
2019-11-29 17:44:32 +0800

    drag and drop now updates in real time

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   get '/showall' => 'appointments#allAppts'
   get '/allpatients' => 'appointments#patients'
   post '/appts' => 'appointments#create'
-  patch '/appts/:id' => 'appointments#update'
+  # resources :appts
+  put '/appts/:id' => 'appointments#update'
 
   resources :patients  
-  resources :appts
+  
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
