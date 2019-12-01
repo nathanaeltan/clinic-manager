632abed01be4bfd838ed091c18919ad49dd4cb2d
Nathanael Tan <nathanael.tan92@gmail.com>
2019-12-01 17:54:59 +0800

    working dropdown selection for medication but in bootstrap 3

--- app/javascript/components/DisplayInfo.jsx
+++ app/javascript/components/DisplayInfo.jsx
@@ -9,11 +9,12 @@ export class DisplayInfo extends Component {
 
         }
     render() {
-        console.log(this.props.displayinfo.extendedProps? this.props.displayinfo.extendedProps.patient_id : null)
+        console.log(this.props.displayinfo)
         return (
             <div>
-                <div className="container text-center">
-                    <p className="border-bottom">{this.props.displayinfo.title } Details </p> 
+                <div className="container text-center border">
+                    <p className="border-bottom"> Details </p> 
+                        <p>{this.props.displayinfo.extendedProps ? "Name: " + this.props.displayinfo.title: ""}</p>
                     <p> {this.props.displayinfo.extendedProps ? "Email: " + this.props.displayinfo.extendedProps.email : ""}</p>
                     <p> {this.props.displayinfo.extendedProps ? "Phone: " + this.props.displayinfo.extendedProps.phone : ""}</p>
                     <p> {this.props.displayinfo.extendedProps ? "Diagnosis: " + this.props.displayinfo.extendedProps.diagnosis : ""}</p>
@@ -21,10 +22,10 @@ export class DisplayInfo extends Component {
                     <p> {this.props.displayinfo.extendedProps === undefined ? "" : this.props.displayinfo.extendedProps.medications.length === 0 ? "" : "Medication"}</p>
                     <ul>    
                          {this.props.displayinfo.extendedProps === undefined ? "" : this.props.displayinfo.extendedProps.medications.map((med, i) => <li key={i}>{med.name}</li>)}
-                         {this.props.displayinfo.extendedProps === undefined ? "" : <button onClick={this.handleDelete}>Delete</button>} 
+                         {this.props.displayinfo.extendedProps === undefined ? "" : <button onClick={this.handleDelete} className="btn btn-danger">Delete</button>} 
 
                         
-                         {this.props.displayinfo.extendedProps? <a href={"patients/" + this.props.displayinfo.extendedProps.patient_id}>Edit</a>  : null}
+                         {this.props.displayinfo.extendedProps? <a href={"patients/" + this.props.displayinfo.extendedProps.patient_id} className="btn btn-primary ml-2">Edit</a>  : null}
                     </ul>
                 </div>
                
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
 
 # patient2 = Patient.create!({:name => 'Patient 1', :phone => '9452919', :email => 'patient1@patient1.com', :diagnosis => 'diabetes', :user_id => 1})
 # Test.create!({:name =>'HBA1C'})
-Test.create!({:name =>'Test 2'})
-Test.create!({:name =>'Test 3'})
-Test.create!({:name =>'Test 4'})
-# newMed = Medication.create!({:name => 'Insulin'})
+# Test.create!({:name =>'Test 2'})
+# Test.create!({:name =>'Test 3'})
+# Test.create!({:name =>'Test 4'})
+Medication.create!({:name => 'Panadol'})
+Medication.create!({:name => 'Dhamotil'})
+Medication.create!({:name => 'AntiBiotics'})
+Medication.create!({:name => 'Diabetes Test kit'})
+
+
 # Med2 = Medication.create!({:name => 'Arcoxia'})
 # appt1 = Appt.create!({:patient_id => 1, :time => '2019-11-30T10:00:00+08:00'})
 # new = MedicationPatient.create!({:medication_id => 1, :patient_id => 1})
